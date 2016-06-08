9657e6958be3bcafa08f2f77d5847c4f2aec2c89
Caleb Knox <knoxcaleb@aol.com>
2016-06-08 22:50:10 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,10 +5,8 @@
 # }
 
   # add your code snippet here!
-  def happy_birthday(birthday_kids)
-    birthday_kids.each do |kids_name, age|
+def happy_birthday(hash)
+  hash.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-
-
